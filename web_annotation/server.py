53b80b08ecc0bb4827aa452057f90a25d7bcc628
Ravinithesh Annapureddy <ravinitheshreddy@gmail.com>
2022-04-08 16:25:00 +0200

    stores the links to disk

--- web_annotation/server.py
+++ web_annotation/server.py
@@ -1,24 +1,16 @@
-import time
-
-# from annotation_loop import main, get_data
-from flask import Flask, redirect, url_for, render_template, request, jsonify, flash
-
-
-import os
-import json
-
+import sys
+from flask import Flask, render_template, request
 from requests import get
 
-# embeddings, data, tree, reverse_map = get_data(size=10000)
+sys.path.insert(0, ".")
 
-# print("len: {}".format(len(embeddings)))
+from annotation_loop import get_links, setup, store_morph
+
+embeddings, data, tree, reverse_map, uid2path = setup(
+    data_dir="./data/", path="./data/", size=10000
+)
 
 app = Flask(__name__)
-
-# current_directory = os.path.dirname(os.path.realpath(__file__))
-# static_dir = os.path.join(current_directory, "static")
-
-PAGE_SIZE = 6
 
 
 @app.route("/")
@@ -36,42 +28,23 @@ def annotate_images():
         if request.form["submit"] in ["text_search", "random_search"]:
             if request.form["submit"] == "text_search":
                 image_uid = request.form["item"]
-            # photos, searched_items, number_of_results = search_photos(
-            #     image_uid, page_number, PAGE_SIZE
-            # )
+            else:
+                image_uid = False
 
-            similar_images = [
-                [
-                    "id1",
-                    f"https://dhlabsrv4.epfl.ch/iiif_replica/cini%2F61A%2F61A_632.jpg/full/300,/0/default.jpg",
-                ],
-                [
-                    "id2",
-                    f"https://dhlabsrv4.epfl.ch/iiif_replica/cini%2F87A%2F87A_507.jpg/full/300,/0/default.jpg",
-                ],
-                [
-                    "id3",
-                    f"https://dhlabsrv4.epfl.ch/iiif_replica/cini%2F1C%2F1C_484.jpg/full/300,/0/default.jpg",
-                ],
-                [
-                    "id4",
-                    f"https://dhlabsrv4.epfl.ch/iiif_replica/cini%2F42C%2F42C_694.jpg/full/300,/0/default.jpg",
-                ],
-                [
-                    "id5",
-                    f"https://dhlabsrv4.epfl.ch/iiif_replica/cini%2F96A%2F96A_336.jpg/full/300,/0/default.jpg",
-                ],
-                [
-                    "id6",
-                    f"https://dhlabsrv4.epfl.ch/iiif_replica/cini%2F7A%2F7A_161.jpg/full/300,/0/default.jpg",
-                ],
-            ]
-            compared_with_img_url = f"https://dhlabsrv4.epfl.ch/iiif_replica/cini%2F97A%2F97A_64.jpg/full/300,/0/default.jpg"
+            compared_image, similar_images = get_links(
+                embeddings, data, tree, reverse_map, uid2path, uid=image_uid, n=7
+            )
+
+            image_uid, compared_with_img_url = compared_image
 
             number_of_results = len(similar_images)
 
         if request.form["submit"] == "similar_images":
-            
+            similar_imges_uids = []
+            for form_key in request.form.keys():
+                if "ckb" in form_key:
+                    similar_imges_uids.append(request.form[form_key])
+            store_morph(request.form["UID_A"], similar_imges_uids, data_dir="./data/")
 
     return render_template(
         "annotate.html",
@@ -80,7 +53,6 @@ def annotate_images():
         number_of_results=number_of_results,
         item=image_uid,
         cold_start=request.method == "GET",
-        page_size=PAGE_SIZE,
     )
 
 
